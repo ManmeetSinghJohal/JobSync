b9f22e50f58620614563de48f27ff210541e92b8
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-21 15:42:17 +0100

    Component to address situation when a path is supplied by api but its not valid or broken

---
+++ components/SafeImage.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+
+interface SafeImageProps {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+    className: string;
+    defaultSrc: string;
+}
+
+const SafeImage = ({ src, alt, width, height, className, defaultSrc }: SafeImageProps) => {
+  return (
+    <Image
+      src={src || defaultSrc}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+      onError={(event) => {
+        event.currentTarget.src = defaultSrc;
+        console.log(defaultSrc);
+      }}
+      unoptimized={true}
+    />
+  );
+};
+
+export default SafeImage;
