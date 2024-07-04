c11468224eb42ccdfd8f2aedb260a21c391ab0f2
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-04 22:49:44 +0100

    Add Head tag with various link tags with favicon images

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Manrope } from "next/font/google";
 import type { Metadata } from "next";
 import { cookies } from "next/headers";
+import Head from "next/head";
 
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
@@ -25,6 +26,26 @@ export default function RootLayout({
   return (
     <ThemeProvider theme={theme}>
       <html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <body className={`${manrope.className} ${theme} `}>
           <main className=" min-h-screen bg-natural-3 px-5 dark:bg-darkBg-1">
             <div className="mx-auto flex max-w-[1280px] flex-col items-center">
